63aa992c514b0ef6d2b63fd2940e3ec211f1db3c
Vishal <70593973+Vishal-sah01@users.noreply.github.com>
2024-09-03 17:27:30 +0530

    Add files via upload

--- resume/src/app/layout.tsx
+++ resume/src/app/layout.tsx
@@ -1,32 +1,15 @@
-import { Footer, Navbar } from "@/components";
-import { SITE_CONFIG } from "@/config";
-import { cn } from "@/lib/utils";
-import "@/styles/globals.css";
-import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
-import { Inter } from "next/font/google";
+import React from 'react'
 
-const font = Inter({ subsets: ["latin"] });
-
-export const metadata = SITE_CONFIG;
-
-export default function RootLayout({
-    children,
-}: {
+interface Props {
     children: React.ReactNode;
-}) {
+}
+
+const AuthLayout = ({ children }: Props) => {
     return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={cn(
-                    "min-h-screen bg-background text-foreground antialiased max-w-full overflow-x-hidden",
-                    font.className
-                )}
-            >
-                <ClerkProvider appearance={{ baseTheme: dark }}>
-                    {children}
-                </ClerkProvider>
-            </body>
-        </html>
-    );
+        <div className="flex items-center justify-center h-screen">
+            {children}
+        </div>
+    )
 };
+
+export default AuthLayout
